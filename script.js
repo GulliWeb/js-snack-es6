9f40a0ff1b6b000651f16e20d831113e39274fc9
Andrea Gulli <andreagulli11@gmail.com>
2024-08-29 17:20:25 +0200

    completed SNACK 2

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 //     'Chiara Ferragni', 'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'
 // ]
 
-// guessList.forEach((guess, i) => {
+// guessList.forEach((guess, i ) => {
 //     // Creo OGGETTI guest ed assegno le relative proprietà
 //         guess = {
 //         tableName : tableName,
@@ -19,6 +19,7 @@
 
 // Snack 1 con metotdo.map()
 // Creo la lista degli invitati
+let consol = document.getElementById('console')
 const tableName = 'Tavolo vip'
 const guessList = [
     'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 
@@ -32,3 +33,30 @@ const guest = guessList.map((guestName, index) => ({
     }
 ));
 console.table(guest)
+
+
+// Snack 2 
+
+// Preparo l'elenco degli studenti
+const student = [
+    { ID: 213, name: 'Marco della Rovere', grades: 78 },
+    { ID: 110, name: 'Paola Cortellessa', grades: 96 },
+    { ID: 250, name: 'Andrea Mantegna', grades: 48 },
+    { ID: 145, name: 'Gaia Borromini', grades: 74 },
+    { ID: 196, name: 'Luigi Grimaldello', grades: 68 },
+    { ID: 102, name: 'Piero della Francesca', grades: 50 },
+    { ID: 120, name: 'Francesca da Polenta', grades: 84 }
+];
+
+// Trasformo i nomi in MAIUSCOLO
+const studentUpperCase = student.map(student => student.name.toUpperCase());
+console.table(studentUpperCase)
+
+// Creo la lista dei soli studenti che hanno voti > 70
+const student70 = student.filter(student => student.grades > 70)
+console.table(student70)
+
+// Creo la lista dei soli studenti che hanno voti > 70 e ID > 120
+const student120 = student.filter(student => student.grades > 70 && student.ID > 120)
+console.table(student120)
+
